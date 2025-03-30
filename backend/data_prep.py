b44eb023e057a1780ed371a2f54ca32c1b58593d
novellaalvina <112059794+novellaalvina@users.noreply.github.com>
2025-03-30 12:07:23 -0600

    system flow update

---
+++ backend/data_prep.py
@@ -0,0 +1,131 @@
+import ast
+import pandas as pd
+import numpy as np
+import datetime
+from IPython.display import display
+from dataprep.clean import clean_date, clean_df, clean_email, clean_address, clean_country, clean_url, clean_phone, clean_isbn, clean_text
+
+def main():
+    
+    # get the data path from the user
+    data_path = input("Enter the data path")
+    data_path = '/Users/novellaalvina/Documents/US/UTAH/Lessons/MS/Spring2025/CS 6964/project/IHCS/potential_dataset/Messy-Data.csv'
+    
+    # create dataframe from the uploaded file
+    md_df = pd.read_csv(data_path)
+    
+    # display the few examples of the data and ask user to verify
+    display(md_df.head(10))
+    user_verification = str(input("Confirm to continue the process with the uploaded data? y/n ")).lower()
+    while user_verification != 'y':
+        data_path = input("Please enter the correct data path")
+        md_df = pd.read_csv(data_path)
+        display(md_df.head(10))
+        user_verification = input("Comfirm to continue the process with the uploaded data? y/n")
+    
+    # formatted the whole dataframe first
+    inferred_dtypes, formatted_df = clean_df(md_df, standardize_missing_values='ignore')
+    
+    # display the data info and example of the formatted data and asked the user to verify
+    display(inferred_dtypes)
+    display(formatted_df.head(10))
+    user_verification = input("Do the formatted data look good? (y/n) ")
+    
+    if user_verification != 'y':
+        custom_column = False
+        # ask the user which columns need to be formatted and their correct datatype
+        column_to_format_validation = False
+        while column_to_format_validation == False:
+            columns_to_format = ast.literal_eval(input("Which columns need to be formatted? (dict = {col: datatype})"))
+            if not isinstance(columns_to_format, dict):
+                print("The columns_to_format has to be a dictionary. Please try again.")
+            else:
+                column_to_format_validation = True
+        
+        # columns_to_format = {'dob': 'datetime', 'join_date': 'datetime', 'email': 'email', 'address': 'US_address'}
+        # columns_to_format = columns_to_format.split(',')
+        # columns_to_format = [col.strip() for col in columns_to_format]
+       
+        # year format checking
+        year_format_validation = False
+        while not year_format_validation:
+            year_format = ast.literal_eval(input("Which columns need to format the year? (dict = {col: {'min': year, 'max': year}})"))
+            if not isinstance(year_format, dict):
+                print("The year_format has to be a dictionary. Please try again.")
+                break
+            # year_format = {'dob': {'min': 1926, 'max': 2025}} # only dob column needs formatting
+        
+            for col in year_format.keys():
+                min_year = year_format[col]['min']
+                max_year = year_format[col]['max']
+                
+                # check for the min and max year null values
+                if min_year is None or max_year is None: break                          # if both min and max year are none, then ask the user to input again
+                if min_year is None and max_year is not None: min_year = max_year - 99  # if the min year is None but the max year is not None, then the min year will be 99 years less than the max year.
+                if max_year is None and min_year is not None: max_year = min_year + 99  # if the max year is None but the min year is not None, then the max year will be 99 years more than the min year.
+                
+                # if the gap between min and max year is greater than or equal to 100, then ask the user to input again
+                if max_year - min_year >= 100:
+                    year_format = ast.literal_eval(input(f"The {col} year range is too large. Please check the year format. Please enter the correct year range. \nWhich columns need to format the year? (dict = {col: {'min': year, 'max': year}})"))
+                    break
+                
+                # if the col is the last column with no 'break', then the year format is valid
+                if col == list(year_format.keys())[-1]: year_format_validation = True
+        
+        """
+        # # text custom pipeline checking
+        # pipeline_validation = False
+        # while not pipeline_validation:
+        #     pipeline_message = f'Customize your text cleaning pipeline following the template in the README.'
+        #     custom_pipeline = input(pipeline_message)
+        #     custom_pipeline = [
+        #         {"operator": "lowercase"},
+        #         {"operator": "remove_digits"},
+        #         {"operator": split},
+        #         {"operator": replace_z, "parameters": {"value": "*"}},
+        #         {"operator": "remove_whitespace"},
+        #     ]
+            
+        #     # check if the custom pipeline follows the template given
+        #     if not isinstance(custom_pipeline, list):
+        #         print("The custom pipeline has to be wrapped in a list as shown in the template. Please try again.")
+        #         break
+        #     else:
+        #         for op in custom_pipeline:
+        #             op = ast.literal_eval(op)
+        #             if not isinstance(op, dict):
+        #                 print("Each operator in the custom pipeline has to be wrapped in a dictionary as shown in the template. Please try again.")
+        #                 break
+        #             else:
+        """            
+        
+        error_list = [] 
+        for col in columns_to_format.keys():
+            data_type = columns_to_format[col]
+            try:
+                if data_type == 'datetime':
+                    formatted_df[col] = clean_date(formatted_df, col, errors='raise', report=True, progress=True)[f'{col}_clean']
+                    formatted_df[col] = pd.to_datetime(formatted_df[col])
+                    if year_format[col]:
+                        formatted_df[col] = formatted_df[col].apply(lambda x: x.replace(year=x.year-100) if x.year > year_format[col]['max'] else x)
+                elif data_type == 'email':
+                    formatted_df[col] = clean_email(formatted_df, col, errors='raise', remove_whitespace=True, report=True, progress=True)[f'{col}_clean']
+                elif data_type == 'US_address':
+                    formatted_df[col] = clean_address(formatted_df, col, errors='raise', report=True, progress=True)[f'{col}_clean']
+                elif data_type == 'country':
+                    formatted_df[col] = clean_country(formatted_df, col, errors='raise', report=True, progress=True)[f'{col}_clean']
+                elif data_type == 'url':
+                    formatted_df[col] = clean_url(formatted_df, col, errors='raise', report=True, progress=True)[f'{col}_clean']
+                elif data_type == 'phone':
+                    formatted_df[col] = clean_phone(formatted_df, col, errors='raise', report=True, progress=True)[f'{col}_clean']
+                elif data_type == 'isbn':
+                    formatted_df[col] = clean_isbn(formatted_df, col, errors='raise', report=True, progress=True)[f'{col}_clean']
+                elif data_type == 'text':
+                    formatted_df[col] = clean_text(formatted_df, col, errors='raise', report=True, progress=True)[f'{col}_clean']
+                else:
+                    custom_column = True
+            except Exception as error:
+                error_list.append(error.__str__)
+                
+if __name__ == '__main__':
+    main()
--- system_flow.md
+++ system_flow.md
@@ -1,16 +1,63 @@
-IDEAS:
+System Flow:
 
 Data Standardizing and Formatting System:
 1. uploaded dirty dataset (converted to csv) first go through the `clean_df` from `dataprep` library which will attempt to format the whole dataset and outputs a tuple of `inferred_dtypes` and `cleaned_df`
 2. based on the outputted `inferred_dtypes`, system then asks the user to verify if all columns in the dataset has the correct data types and `clean_df` is supposedly the clean version of the dataset should look like.
-3. if yes, then we can proceed to standardizing process. if not then, the system asks the users to identify which columns are incorrect and have automatic clean up for columns of date/time, email, country, duplicate values, phone number, text, US street address, url, ISBN numbers. if the incorrect columns are not part of the mentioned types, then have user input the desired pattern for those columns and detected the non-matching pattern values for those columns and asks the user on how to correct them.
+3. if yes, then we can proceed to standardizing process. if not then, the system asks the users to identify which columns are incorrect (sent to the backend as a dictionary where the key is the column name and the value is the datatype for their respective column) and have automatic clean up for columns of:
+  - date/time, 
+  - email, 
+  - country, 
+  - duplicate values, 
+  - phone number (if include country code, then US only), 
+  - text, 
+  - US street address, 
+  - url, 
+  - ISBN numbers. 
 
-columns params:
+If the incorrect columns are not part of the mentioned types, then have user input the desired pattern for those columns and detected the non-matching pattern values for those columns and asks the user on how to correct them. 
+
+If there exists any invalid parsing in the automatic clean up, then the system will accumulate the errors and asked the users on how to handle the invalid parsing.
+
+Note on columns options:
 1. date
-    - year format checkbox with max_year fill-in box
+    - `year_format` checkbox with `max_year` and `min_year` fill-in box
       
       info: this is needed to identify the year of the form YY that might be ambigous. 
       i.e if the range is between 1900s and 2000s and the year is 22, the system will automatically recognized it as 2022, unless this checkbox is checked.
       
-      if yes, then those year 
-    
+      if the `year_format` checkbox is checked, the system will ask the user to specify the range of years that the dataset covers (the minimum(`min_year`) and maximum(`max_year`) year of the date column covers). The gap or difference between the `min_year` and `max_year` less than 100 years. If both `min_year` and `max_year` are None (no input), then it is considered an invalid input and the system will ask the user to specify the year range again. If `min_year` year has no input but the `max_year` has input, then the default value for the `min_year` is `max_year`-99 and vice versa. 
+      
+      For example:
+      - `min_year` = 1900 and `max_year` = 2000, this will result in warning and make the user to input the correct range of years.
+      - `min_year` = 1926 and `max_year` = 2025, then if YY <= 25, then YYYY = (2000-2025) and if YY > 25, then YYYY = (1926-1999). i.e YY=22 will be converted to 2022 and YY=30 will be converted to 1930.
+
+2. Text
+The default pipeline for the clean_text() function is the following: (taken from dataprep API Documentation)
+  - fillna: Replace all null values with NaN.
+  - lowercase: Convert all characters to lowercase.
+  - remove_digits: Remove numbers.
+  - remove_html Remove HTML tags.
+  - remove_urls: Remove URLs.
+  - remove_punctuation: Remove punctuation marks.
+  - remove_accents: Remove accent marks.
+  - remove_stopwords: Remove stopwords.
+  - remove_whitespace: Remove extra spaces, and tabs and newlines.
+
+<!-- The pipeline can be customized by filling in the customized pipeline box. The custom pipeline can take actions from the list above or custom functions from the user. Like the following:
+```python
+import re
+
+def split(text: str) -> str:
+    return str(text).split()
+
+def replace_z(text: str, value: str) -> str:
+    return re.sub(r"z", value, str(text), flags=re.I)
+
+custom_pipeline = [
+    {"operator": "lowercase"},
+    {"operator": "remove_digits"},
+    {"operator": split},
+    {"operator": replace_z, "parameters": {"value": "*"}},
+    {"operator": "remove_whitespace"},
+]
+``` -->
